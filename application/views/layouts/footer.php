7eb0cab37b77fd314fdc222de16a4ba000769c2c
dixroby <alestilodefe@gmail.com>
2019-07-07 05:00:32 -0500

    inicio Sistema de ventas desde Cero con CodeIgniter

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,29 @@
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 2.4.0
+            </div>
+            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+            reserved.
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+<!-- jQuery 3 -->
+<script src="<?php echo base_url();?>assets/template/jquery/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url();?>assets/template/bootstrap/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url();?>assets/template/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- FastClick -->
+<script src="<?php echo base_url();?>assets/template/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>assets/template/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>assets/template/dist/js/demo.js"></script>
+<script>
+$(document).ready(function () {
+$('.sidebar-menu').tree()
+})
+</script>
+</body>
+</html>
